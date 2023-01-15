95f3acf49b36d21783f54f3e83bd5350d84788cd
monblank <monblank@localhost>
2023-01-15 22:50:09 +0500

    HW Done

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+    	int counter = 0;
+    	while (counter < 100) {
+    		counter = counter + 2;
+    		System.out.println("Уволен айтишник с id " + counter);
+    	}
     }
 }
